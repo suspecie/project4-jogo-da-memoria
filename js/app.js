5e26df3c4a671c51b4d57c723fcff15b1ebd635e
Suellyn Specie <sspecie13@gmail.com>
2019-05-01 23:53:30 -0300

    funcao para comparar as cartas

--- js/app.js
+++ js/app.js
@@ -1,4 +1,12 @@
 /**
+ * Variavel Global
+ */
+let lastCardCliked = '';
+let idLastCardClicked = '';
+let cardsShow = 0;
+
+
+/**
  * Embaralha o array
  * Método utilizado do http://stackoverflow.com/a/2450976
  */
@@ -40,6 +48,47 @@ function addClassOrder() {
     });
 }
 
+/**
+ * Verifica se a dupla de cartas são iguais.
+ * Se for igual muda a cor para verde e deixa as cartas para cima.
+ * Se não for igual vira a carta para baixo.
+ * Se virou duas cartas e não são iguais vira a dupla para baixo.
+ */
+function compareCoupleCards(idCard, cardClicked) {
+    cardsShow++;
+    if (cardsShow <= 1) {
+        idLastCardClicked = idCard;
+        lastCardCliked = cardClicked;
+        console.log('cliquei a primeira vez', lastCardCliked);
+    } else {
+        if (lastCardCliked === cardClicked) {
+            $(`#${idCard}`).addClass('match');
+            $(`#${idLastCardClicked}`).addClass('match');
+
+            console.log('é igual');
+        } else {
+            setTimeout(function () {
+                console.log('entrei no setimeout');
+                $(`#${idCard}`).removeClass('open');
+                $(`#${idLastCardClicked}`).removeClass('open');
+                lastCardCliked = '';
+                idLastCardClicked = '';
+                cardsShow = 0;
+            }, 2000);
+            console.log('nao é igual');
+        }
+    }
+}
+
+/**
+ * Mostra a carta e compara se é igual
+ */
+$('.card').click(function (evt) {
+    const id = $(this).attr('id');
+    $(`#${id}`).addClass('open');
+    const iconClicked = $(`#${id}`).children().attr('class');
+    compareCoupleCards(id, iconClicked);
+});
 
 /**
  * Inicia o Jogo embaralhando as cartas
@@ -47,3 +96,4 @@ function addClassOrder() {
 $(function () {
     addClassOrder();
 });
+
